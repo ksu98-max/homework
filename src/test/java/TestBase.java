e133025d22f3d6bd1572bc0d3c4df0e447dd69a4
Ksenia Mal <ksu.98@inbox.ru>
2026-02-17 16:40:35 +0300

    Trying to run test 1

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,12 +1,29 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
-
         @BeforeAll
-        static void  beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.browserSize = "1920х1080";
-        Configuration.timeout = 10000;
-    }
+        static void setUp() {
+                Configuration.browserSize = "1920x1080";
+                Configuration.pageLoadStrategy = "eager";
+                Configuration.baseUrl = "https://demoqa.com";
+        }
+
+        @Test
+        void checkPracticeFormTest() {
+                open("");
+                $$(".card-body").findBy(text("Forms")).click();
+                $$(".router-link").findBy(text("Practice Form")).click();
+                //  @BeforeAll
+                //  static void  beforeAll() {
+                //   Configuration.baseUrl = "https://demoqa.com";
+                //   Configuration.browserSize = "1280x1024";
+                //   Configuration.timeout = 10000;
+                //  Configuration.pageLoadStrategy = "eager";
+        }
 }
--- src/test/java/test_one.java
+++ src/test/java/test_one.java
@@ -1,21 +1,59 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import java.nio.channels.ConnectionPendingException;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static java.awt.SystemColor.text;
 
 public class test_one extends TestBase {
       @Test
-         void successfulFillFormTest() {
-open("automation-practice-form");
-      $("#firstName").setValue("ksenia");
-      $("#lastName").setValue("mal");
-      $("#userEmail").setValue("msl@t.ru");
-      $("#id=gender-radio-2").setValue("Female");
-      $("#userNumber").setValue("2569854785");
-      $("#id=submit").click();
+      void successfulFillFormTest() {
+            open("/automation-practice-form");
+            $("#firstName").setValue("ksenia");
+            $("#lastName").setValue("mal");
+            $("#userEmail").setValue("msl@t.ru");
+            $("#genterWrapper").$(byText("Female")).click();
+            $("#userNumber").setValue("2566587452");
+            $("#dateOfBirthInput").click();
+            $("#dateOfBirthInput").clear();
+            $("#dateOfBirthInput").sendKeys("17 Feb 2026");
+            $("#subjects-label").setValue("test");
+            $("#uploadPicture").uploadFromClasspath("resources/pic_2.jpg");
+            $("#hobbiesWrapper").$(byText("Music")).click();
+            $("#state").click();
+            $(byText("NCR")).click();
+            $("#city").click();
+            $(byText("Noida")).click();
+            $("#submit").click();
+
+            $("example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("ksenia mal"));
+            $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("msl@t.ru"));
+            $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+            $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("2569854785"));
+            $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("17 Feb 2026"));
+            $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("test"));
+            $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Music"));
+            $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("pic_2.jpg"));
+            $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Noida"));
+      }
+
+      @Test
+      void simpleTest() {
+
+                  $("#firstName").setValue("ksenia");
+                  $("#lastName").setValue("mal");
+                  $("#genterWrapper").$(byText("Female")).click();
+                  $("#userNumber").setValue("2566587452");
+                  $("#dateOfBirthInput").setValue("17 Feb 2026");
+                  $("#submit").click();
+
+                  $(".modal-title").shouldHave(text("Thanks for submitting the form"));
       }
 }
