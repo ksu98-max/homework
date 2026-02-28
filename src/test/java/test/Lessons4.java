f06597e1c9ca2718ac370072ebceaebcad702b96
Ksenia Mal <ksu.98@inbox.ru>
2026-02-28 22:04:05 +0300

    Доработала и запустила тест для урока 4

--- src/test/java/test/Lessons4.java
+++ src/test/java/test/Lessons4.java
@@ -4,45 +4,42 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static testData.TestData.*;
 
 
 public class Lessons4 extends TestBase {
     @Test
-    void successfulFillFormTest() {
-        open("/automation-practice-form");
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(userEmail);
-        $("#genterWrapper").$(byText("Female")).click();
-        $("#userNumber").setValue(userNumber);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("Feb");
-        $(".react-datepicker__year-select").selectOption("2026");
-        $(".react-datepicker__day--017").click();
-        $("#subjectsInput").setValue("English").pressEnter();
-        $("#uploadPicture").uploadFromClasspath("pic_2.jpg");
-        $("#hobbiesWrapper").$(byText("Music")).click();
-        $("#currentAddress").setValue(currentAddress).pressEnter();
-        $("#state").click();
-        $(byText("NCR")).click();
-        $("#city").click();
-        $(byText("Noida")).click();
-        $("#submit").click();
+void successfulFillFormTest() {
+    open("/automation-practice-form");
+    $("#firstName").setValue(firstName);
+    $("#lastName").setValue(lastName);
+    $("#userEmail").setValue(userEmail);
+    $("#genterWrapper").$(byText("Female")).click();
+    $("#userNumber").setValue(userNumber);
+    $("#dateOfBirthInput").click();
+    $(".react-datepicker__month-select").selectOption("February");
+    $(".react-datepicker__year-select").selectOption("2026");
+    $$(".react-datepicker__day").findBy(text("17")).click();
+    $("#subjectsInput").setValue("English").pressEnter();
+    $("#hobbiesWrapper").$(byText("Music")).click();
+    $("#uploadPicture").uploadFromClasspath("pic_2.jpg");
+    $("#currentAddress").setValue(currentAddress).pressEnter();
+    $("#state").click();
+    $(byText("NCR")).click();
+    $("#city").click();
+    $(byText("Noida")).click();
+    $("#submit").click();
 
-        $("example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(firstName + " " + lastName));
-        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text(userEmail));
-        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
-        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(userNumber));
-        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("17 Feb 2026"));
-        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("English"));
-        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Music"));
-        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("pic_2.jpg"));
-        $(".table-responsive").$(byText("currentAddress")).parent().shouldHave(text(currentAddress));
-        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Noida"));
-    }
-
-}
+    $(".modal-content").shouldHave(text("Thanks for submitting the form"));
+    $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(firstName + " " + lastName));
+    $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text(userEmail));
+    $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+    $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(userNumber));
+    $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("17 February,2026"));
+    $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("English"));
+    $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Music"));
+    $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("pic_2.jpg"));
+    $(".table-responsive").$(byText("Address")).parent().shouldHave(text(currentAddress));
+    $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Noida"));
+}}
