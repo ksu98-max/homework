45f9ede6c772df4f735338bb246cb81a82b8563f
Ksenia Mal <ksu.98@inbox.ru>
2026-03-24 22:17:59 +0300

    ДЗ 6 урока.

--- src/test/java/test/Lesson6.java
+++ src/test/java/test/Lesson6.java
@@ -2,13 +2,16 @@ package test;
 
 import com.github.javafaker.Address;
 import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.PracticeForm;
 import testData.TestBase;
 
 import java.util.Locale;
 
-import static Utils.randomUtils.getRandomString;
+import static Utils.randomUtils.*;
+import static Utils.randomUtils.getRandomHobbies;
 import static testData.TestData.*;
 import static testData.TestData.city;
 import static testData.TestData.currentAddress;
@@ -22,8 +25,8 @@ import static testData.TestData.year;
 
 public class Lesson6 extends TestBase {
     PracticeForm practiceForm = new PracticeForm();
-  Faker faker = new Faker();
-   Faker fakerRu = new Faker(new Locale("ru"));
+    Faker faker = new Faker();
+    Faker fakerRu = new Faker(new Locale("ru"));
 
     @Test
     void Test_with_faker() {
@@ -33,7 +36,26 @@ public class Lesson6 extends TestBase {
         String userEmail = faker.internet().emailAddress();
         String userNumber = faker.numerify("##########");
         String currentAddress = fakerRu.address().fullAddress();
-//        String setDateOfBirght = faker.date()
+        String genderFaker = faker.options().option("Male", "Female", "Other");
+        String dayFaker = String.valueOf(faker.number().numberBetween(1, 28));
+        String monthFaker = faker.options().option("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        String yearFaker = String.valueOf(faker.number().numberBetween(1998, 2025));
+        String stateFaker = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
+        String cityFaker="";
+        if (stateFaker.equals("NCR")) {
+            cityFaker = faker.options().option("Delhi", "Gurgaon", "Noida");
+        }
+        if (stateFaker.equals("Uttar Pradesh")) {
+            cityFaker = faker.options().option("Agra", "Lucknow", "Merrut");
+        }
+        if (stateFaker.equals("Haryana")) {
+            cityFaker = faker.options().option("Karnal", "Panipat");
+        }
+        if (stateFaker.equals("Rajasthan")) {
+            cityFaker = faker.options().option("Jaipur", "Jaselmer");
+        }
+        String languageFaker = faker.options().option("English", "Chemistry", "Arts", "Maths", "Biology", "Hindi", "Physics", "Accounting");
+        String hobbiesFaker = faker.options().option("Sports", "Reading", "Music");
 
 
         practiceForm.openPage()
@@ -41,47 +63,17 @@ public class Lesson6 extends TestBase {
                 .typeLastName(lastName)
                 .typeUserEmai(userEmail)
                 .typeUserNumber(userNumber)
-                .setGender(gender)
-                .setDateOfBirght(day, month,year)
-                .typeSubjectSelect(language)
-                .submitHobbies(hobbies)
+                .setGender(genderFaker)
+                .setDateOfBirght(dayFaker, monthFaker, yearFaker)
+                .typeSubjectSelect(languageFaker)
+                .submitHobbies(hobbiesFaker)
                 .file()
                 .typeCurrentAddress(currentAddress)
                 .typeState()
-                .SetStateValue(state)
+                .SetStateValue(stateFaker)
                 .typeCity()
-                .SetCityValue(city)
+                .SetCityValue(cityFaker)
                 .resultFormButton()
                 .CheckField();
     }
-
-    @Test
-    void Test_with_utils() {
-
-        String firstName = getRandomString(10);
-        String lastName = getRandomString(10);
-        String userEmail
-        String userNumber = getRandomString(10);
-        String currentAddress
-//        String setDateOfBirght = faker.date()
-
-
-        practiceForm.openPage()
-                .typeFirstName(firstName)
-                .typeLastName(lastName)
-                .typeUserEmai(userEmail)
-                .typeUserNumber(userNumber)
-                .setGender(gender)
-                .setDateOfBirght(day, month,year)
-                .typeSubjectSelect(language)
-                .submitHobbies(hobbies)
-                .file()
-                .typeCurrentAddress(currentAddress)
-                .typeState()
-                .SetStateValue(state)
-                .typeCity()
-                .SetCityValue(city)
-                .resultFormButton()
-                .CheckField();
-
 }
